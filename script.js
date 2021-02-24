481e0388ddd7681ee75a3cc30d34a37c02513321
Pauan8 <pauline.j.andersson@vgregion.se>
2021-02-24 16:05:04 +0100

    styling, filtering of recipes for startscreen

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const checkbox = document.querySelectorAll(".checkbox")
 const sliderBtnL = document.querySelectorAll(".recipe-slider__btn-left")
 const sliderBtnR = document.querySelectorAll(".recipe-slider__btn-right")
 const recipeSlider = document.getElementById("recipeSlider")
-const topRecipesSlider = document.getElementById("topRecipeSlider")
+const simpleRecipesSlider = document.getElementById("simpleRecipeSlider")
 const fastRecipesSlider = document.getElementById("fastRecipeSlider")
 
 
@@ -16,57 +16,36 @@ const handleUserInput = (input) => {
   filterRecipes(APIurl)
 }
 
-const fetchRecipes = (url) => {
+const fetchRecipes = (url, sender) => {
   fetch(url)
     .then((response) => response.json())
     .then((recipeData) => {
-      displayRecipes(recipeData)
-      generateStartSite(recipeData)
+      switch (sender) {
+        case simpleRecipesSlider:
+        displayRecipes(recipeData, simpleRecipesSlider)
+        break;
+        case fastRecipesSlider:
+        displayRecipes(recipeData, fastRecipesSlider)
+        break;
+        default:
+          displayRecipes(recipeData, recipeSlider)
+      }
+  
     })
+    .catch((error) => alert("Error"))
 }
 
-const generateStartSite = (recipeData) => {
-
-  const displayFastRecipes = (recipes) => {
-    recipes.hits.forEach((item) => {
-      fastRecipesSlider.innerHTML += `
-      <div class="recipe-card__fastFood">
-        <div class="img-container">
-          <img src="${item.recipe.image}"/>
-        </div>
-        <span class="img-text">
-          ${item.recipe.label}
-        </span>
-      </div>
-    `
-    })
-    recipeCard = document.querySelectorAll(".recipe-card__fastFood")
-    recipeCard[0].classList.add("active")
-    arrowButtons(recipeCard, "2")
-   
-  }
-
-  const displayTopRecipes = (recipes) => {
-    recipes.hits.forEach((item) => {
-      topRecipesSlider.innerHTML += `
-      <div class="recipe-card__topFood">
-        <div class="img-container">
-          <img src="${item.recipe.image}"/>
-        </div>
-        <span class="img-text">
-          ${item.recipe.label}
-        </span>
-      </div>
-    `
-    })
-    recipeCard = document.querySelectorAll(".recipe-card__topFood")
-    recipeCard[0].classList.add("active")
-    arrowButtons(recipeCard, "1")
-   
-  }
-  displayFastRecipes(recipeData)
-  displayTopRecipes(recipeData)
+const generateStartSite = () => {
+  const min = Math.ceil(1);
+  const max = Math.floor(100);
+  let randomNumber = Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+  
+  const simpleFoodsUrl = `https://api.edamam.com/search?q=&app_id=38a129f8&app_key=ad250481ec39e7ffc0c0904ddbc693f8&ingr=5&from=${randomNumber}` 
+  const fastFoodUrl = `https://api.edamam.com/search?q=&app_id=38a129f8&app_key=ad250481ec39e7ffc0c0904ddbc693f8&time=15-30&from=${randomNumber}`
+  fetchRecipes(simpleFoodsUrl, simpleRecipesSlider)
+  fetchRecipes(fastFoodUrl, fastRecipesSlider)
 }
+
 
 const filterRecipes = (url) => {
   let filteredUrl = url
@@ -75,51 +54,72 @@ const filterRecipes = (url) => {
     checked.forEach((filter) => {
       filteredUrl += `&health=${filter.value}`
     })
-    fetchRecipes(filteredUrl)
+    fetchRecipes(filteredUrl, "user")
   } else {
-    fetchRecipes(url)
+    fetchRecipes(url, "user")
   }
 }
 
-const displayRecipes = (recipes) => {
-  let i = 0;
+/*Displays recipes in different innerHTML sliders depending on arguments so
+several different slideshows can be active at the same time */
+const displayRecipes = (recipes, container) => {
+let divclass, index;
+
+  switch (container) {
+    case simpleRecipesSlider:
+      container.classList.add("active")
+      index = 1;
+      divclass = "recipe-card__simpleFood"
+      break;
+    case fastRecipesSlider:
+      container.classList.add("active")
+      index = 2;
+      divclass = "recipe-card__fastFood"
+      break;
+    default:
+      container.classList.add("active")
+      index = 0;
+      divclass = "recipe-card__userSearch"
+  }
+
   recipes.hits.forEach((item) => {
-    recipeSlider.innerHTML += `
-      <div class="recipe-card__userSearch">
-        <div class="img-container">
-          <img src="${item.recipe.image}"/>
-        </div>
-        <span class="img-text">
-          ${item.recipe.label}
-        </span>
+    container.innerHTML += `
+    <div class="${divclass}">
+      <div class="img-container">
+        <img src="${item.recipe.image}"/>
       </div>
-    `
+      <span class="img-text">
+        ${item.recipe.label}
+      </span>
+    </div>
+  `
   })
+  recipeCard = document.querySelectorAll(`.${divclass}`)
+  recipeCard[0].classList.add("active")
 
-  recipeCard = document.querySelectorAll(".recipe-card__userSearch")
-  recipeCard[i].classList.add("active")
-  arrowButtons(recipeCard, "0")
-}
-
-
-const arrowButtons = (card, number) => {
-  i = 0
-  sliderBtnL[number].addEventListener("click", () => {
-    card[i].classList.remove("active")
-    i > 0 ? i-- : i = 9
-    card[i].classList.add("active")
-  })
-  sliderBtnR[number].addEventListener("click", () => {
-    card[i].classList.remove("active")
-    i === 9 ? i = 0 : i++
-    card[i].classList.add("active")
-  })
+  /*Connects the arrowbuttons to the correct innerHTML elements and makes them function 
+  as right/left button to view slideshow of foods */
+  const arrowButtons = (card, index) => { 
+    let i =0;
+    sliderBtnL[index].addEventListener("click", () => {
+      card[i].classList.remove("active")
+      i > 0 ? i-- : i = 9
+      card[i].classList.add("active")
+    })
+    sliderBtnR[index].addEventListener("click", () => {
+      card[i].classList.remove("active")
+      i === 9 ? i = 0 : i++
+      card[i].classList.add("active")
+    })
+  }
+  
+  arrowButtons(recipeCard, index)
 }
 
 const clearAll = () => {
   userInput.value = ""
   checkbox.forEach(item => item.checked = false);
-  recipeSlider.innerHTML = ""
+  recipeSlider.innerHTML = `<span class="recipe-slider__heading">Your search hits</span>`
 }
 
 //eventlisteners
@@ -137,3 +137,5 @@ searchBtn.addEventListener("click", (event) => {
   handleUserInput(userInput.value)
   clearAll()
 })
+
+generateStartSite()
